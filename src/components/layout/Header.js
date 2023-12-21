448211962021e34c70f6ebee1fd7c74d41567077
Walter Manqui <wmanqui@gmail.com>
2023-12-21 10:53:53 -0300

    Punto inicial del prouecto

---
+++ src/components/layout/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+/* import { Button } from 'reactstrap';*/
+
+const Header = (props) => {
+    return(
+    <div>Header Walter</div>
+    );
+}
+
+export default Header;
+
+
